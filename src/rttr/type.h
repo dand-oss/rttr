02d0b9003aa9714a042608a83cdd1dab5516aa66
Dan Dees <dand@appsmiths.com>
2018-06-16 08:25:39 +0700

    expose create_variant - not working!

--- src/rttr/type.h
+++ src/rttr/type.h
@@ -1111,6 +1111,9 @@ class RTTR_API type
         template<typename T>
         static void register_less_than_comparator();
 
+        //! Creates a variant from the given argument data.
+        RTTR_INLINE variant create_variant(const argument& data) const;
+
     private:
 
         /*!
@@ -1207,9 +1210,6 @@ class RTTR_API type
         /////////////////////////////////////////////////////////////////////////////////
         /////////////////////////////////////////////////////////////////////////////////
         /////////////////////////////////////////////////////////////////////////////////
-
-        //! Creates a variant from the given argument data.
-        RTTR_INLINE variant create_variant(const argument& data) const;
 
         friend class variant;
         template<typename Target_Type, typename Source_Type>
